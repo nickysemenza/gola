8796b0117cb1ce936fe91d87af6b4ac0213159a1
Nicky Semenza <nicky@nickysemenza.com>
2018-01-05 11:34:01 -0800

    clean up structure

--- client.go
+++ client.go
@@ -4,52 +4,71 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/nickysemenza/gola/ola_proto"
 	"net"
+	"log"
 )
 
-func GetPlugins(conn net.Conn) (resp *ola_proto.PluginListReply, err error) {
+type OlaClient struct {
+	Conn net.Conn
+	Address string
+}
+
+func New(address string) *OlaClient {
+	conn, err := net.Dial("tcp", "localhost:9010")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &OlaClient{
+		Address: address,
+		Conn: conn,
+	}
+}
+func (o *OlaClient) Close() {
+	o.Conn.Close()
+}
+func (o *OlaClient) GetPlugins() (resp *ola_proto.PluginListReply, err error) {
 
 	req := new(ola_proto.PluginListRequest)
 	resp = new(ola_proto.PluginListReply)
 
-	err = callRpcMethod(conn, "GetPlugins", req, resp)
+	err = callRpcMethod(o.Conn, "GetPlugins", req, resp)
 	return
 }
-func GetUniverseList(conn net.Conn) (resp *ola_proto.UniverseInfoReply, err error) {
+func (o *OlaClient) GetUniverseList() (resp *ola_proto.UniverseInfoReply, err error) {
 
 	req := new(ola_proto.OptionalUniverseRequest)
 	resp = new(ola_proto.UniverseInfoReply)
 
-	err = callRpcMethod(conn, "GetUniverseInfo", req, resp)
+	err = callRpcMethod(o.Conn, "GetUniverseInfo", req, resp)
 	return
 }
-func GetUniverseInfo(conn net.Conn, universe int) (resp *ola_proto.UniverseInfoReply, err error) {
+func (o *OlaClient) GetUniverseInfo( universe int) (resp *ola_proto.UniverseInfoReply, err error) {
 
 	req := new(ola_proto.OptionalUniverseRequest)
 	resp = new(ola_proto.UniverseInfoReply)
 
 	req.Universe = proto.Int(universe)
-	err = callRpcMethod(conn, "GetUniverseInfo", req, resp)
+	err = callRpcMethod(o.Conn, "GetUniverseInfo", req, resp)
 	return
 }
 
-func GetDmx(conn net.Conn, universe int) (resp *ola_proto.DmxData, err error) {
+func (o *OlaClient) GetDmx( universe int) (resp *ola_proto.DmxData, err error) {
 
 	req := new(ola_proto.UniverseRequest)
 	resp = new(ola_proto.DmxData)
 
 	req.Universe = proto.Int(universe)
-	err = callRpcMethod(conn, "GetDmx", req, resp)
+	err = callRpcMethod(o.Conn, "GetDmx", req, resp)
 	return
 }
 
-func SendDmx(conn net.Conn, universe int, values []byte) (status bool, err error) {
+func (o *OlaClient) SendDmx(universe int, values []byte) (status bool, err error) {
 
 	req := new(ola_proto.DmxData)
 	resp := new(ola_proto.Ack)
 
 	req.Universe = proto.Int(universe)
 	req.Data = values
-	err = callRpcMethod(conn, "UpdateDmxData", req, resp)
+	err = callRpcMethod(o.Conn, "UpdateDmxData", req, resp)
 
 	return true, err
 }
--- main.go
+++ main.go
@@ -2,61 +2,58 @@ package main
 
 import (
 	"log"
-	"net"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	conn, err := net.Dial("tcp", "localhost:9010")
-	defer conn.Close()
-	if err != nil {
-		log.Fatalln(err)
-	}
 
-	if x, err := GetPlugins(conn); err != nil {
+	client := New("localhost:9010")
+	defer client.Close()
+
+	if x, err := client.GetPlugins(); err != nil {
 		log.Printf("GetPlugins: %v", err)
 	}  else {
 		log.Printf("GetPlugins: %v", x)
 	}
 
-	if x, err := GetUniverseList(conn); err != nil {
+	if x, err := client.GetUniverseList(); err != nil {
 		log.Printf("GetUniverseList: %v", err)
 	}  else {
 		log.Printf("GetUniverseList: %v", x)
 	}
 
-	if x, err := GetUniverseInfo(conn, 1); err != nil {
+	if x, err := client.GetUniverseInfo(1); err != nil {
 		log.Printf("GetUniverseInfo: 1: %v", err)
 	}  else {
 		log.Printf("GetUniverseInfo: 1: %v", x)
 	}
 
-	if x, err := GetUniverseInfo(conn, 3); err != nil {
+	if x, err := client.GetUniverseInfo(3); err != nil {
 		log.Printf("GetUniverseInfo: 3: %v", err)
 	}  else {
 		log.Printf("GetUniverseInfo: 3: %v", x)
 	}
 
-	if x, err := GetDmx(conn, 1); err != nil {
+	if x, err := client.GetDmx(1); err != nil {
 		log.Printf("GetDmx: 1: %v", err)
 	}  else {
 		log.Printf("GetDmx: 1: %v", x.Data)
 	}
 
-	if x, err := GetDmx(conn, 3); err != nil {
+	if x, err := client.GetDmx(3); err != nil {
 		log.Printf("GetDmx: 3: %v", err)
 	}  else {
 		log.Printf("GetDmx: 3: %v", x.Data)
 	}
 
-	if x, err := SendDmx(conn, 1, []byte{2, 3, 5, 7, 11, 13, 255, 0}); err != nil {
+	if x, err := client.SendDmx(1, []byte{2, 3, 5, 7, 11, 13, 255, 0}); err != nil {
 		log.Printf("SendDmx: 1: %v", err)
 	}  else {
 		log.Printf("SendDmx: 1: %v", x)
 	}
 
-	if x, err := SendDmx(conn, 3, []byte{2, 3, 5, 7, 11, 13, 255, 0}); err != nil {
+	if x, err := client.SendDmx(3, []byte{2, 3, 5, 7, 11, 13, 255, 0}); err != nil {
 		log.Printf("SendDmx: 3: %v", err)
 	}  else {
 		log.Printf("SendDmx: 3: %v", x)
